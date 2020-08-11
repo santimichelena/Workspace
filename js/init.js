64cf7ae55304600d94bf63e88478fa922cd33b30
santimichelena <68866518+santimichelena@users.noreply.github.com>
2020-08-11 18:52:44 -0300

    workspace

--- js/init.js
+++ js/init.js
@@ -43,7 +43,13 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCTS_URL).then(function(resultObj){
+      if(resultObj.status === "ok")
+      {
+          productsurlArray = resultObj.data;
+          showItemsList(productsurlArray)
+      }
+  });
+      });
   
-
-});
