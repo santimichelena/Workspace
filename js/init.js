0d67859c987a32d9bd586cae2c7a986a89e0afd5
santimichelena <68866518+santimichelena@users.noreply.github.com>
2020-08-11 19:43:23 -0300

    workspace

--- js/init.js
+++ js/init.js
@@ -1,12 +1,4 @@
-const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
-const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
-const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
 const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
-const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
-const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
-const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
-
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -44,12 +36,5 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-  getJSONData(PRODUCTS_URL).then(function(resultObj){
-      if(resultObj.status === "ok")
-      {
-          productsurlArray = resultObj.data;
-          showItemsList(productsurlArray)
-      }
-  });
-      });
+ 
   
