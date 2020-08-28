2298ff3597b35a84da5bf92f1adfeb780fb83ca7
santimichelena <68866518+santimichelena@users.noreply.github.com>
2020-08-28 17:47:27 -0300

    workspace

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function(e){
           
                        localStorage.setItem('User-Logged', JSON.stringify({ email: inputEmail.value}));
                   
-                       window.location = 'Inicio.html';
+                       window.location = 'inicio.html';
         }
     });
 
